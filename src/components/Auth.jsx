bb177d51ff198d8aebca4f28a23c59adffa0934a
Cullen <cullenkuch@gmail.com>
2025-02-16 11:56:25 -0600

    github secrets defined

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -71,7 +71,7 @@ function Auth() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
         <h3 className="text-2xl font-bold text-center">
-          {isLogin ? "Login :(" : "Sign Up"}
+          {isLogin ? "Login" : "Sign Up"}
         </h3>
         <form onSubmit={handleAuth}>
           <div className="mt-4">
